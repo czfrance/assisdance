3c12a81eb230c187d43602bf089c2e396503d2a2
Cynthia France <cf219@duke.edu>
2023-10-04 04:03:01 -0400

    initial git commit with project

---
+++ assisdance/assisdanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  assisdanceApp.swift
+//  assisdance
+//
+//  Created by Cynthia Z France on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct assisdanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
