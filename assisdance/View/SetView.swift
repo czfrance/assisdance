df528de0a18bac496605030aeaf8801adcab51d0
Cynthia France <cf219@duke.edu>
2023-10-05 01:34:41 -0400

    added image resources

---
+++ assisdance/View/SetView.swift
@@ -0,0 +1,20 @@
+//
+//  SetView.swift
+//  assisdance
+//
+//  Created by Cynthia Z France on 10/5/23.
+//
+
+import SwiftUI
+
+struct SetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SetView_Previews: PreviewProvider {
+    static var previews: some View {
+        SetView()
+    }
+}
