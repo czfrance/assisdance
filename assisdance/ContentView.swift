c4b772310a355a96fe96c4e44b70aef9f83a032b
Cynthia France <cf219@duke.edu>
2023-11-18 13:11:29 -0500

    add in saving, loading, decoding

--- assisdance/ContentView.swift
+++ assisdance/ContentView.swift
@@ -9,19 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        VStack {
-//            RootScreen()
-//                .environmentObject(Auth.shared)
-//        }
-//        .padding()
-//        DrawingView()
         VStack {
-            DrawingView()
-            ZStack {
-                DancerIcon(posx: 0.0, posy: 0.0)
-                DancerIcon(posx: 100.0, posy: 100.0)
-            }
+            RootScreen()
+                .environmentObject(Auth.shared)
         }
+        .padding()
+        
+//        DrawingView()
+        
+//        VStack {
+//            DrawingView()
+//            ZStack {
+//                DancerIcon(posx: 0.0, posy: 0.0)
+//                DancerIcon(posx: 100.0, posy: 100.0)
+//            }
+//        }
     }
 }
 //profric
