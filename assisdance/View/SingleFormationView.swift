da283c1cd143dc8cef454c577f78941b775631d9
Cynthia France <cf219@duke.edu>
2023-11-18 13:10:29 -0500

    create stage on which dancers will be placed

--- assisdance/View/SingleFormationView.swift
+++ assisdance/View/SingleFormationView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct SingleFormationView: View {
+    @EnvironmentObject var formationBook: FormationBookViewModel
+    @State var formation: FormationModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(formation.dancers) { dancer in
+                    DancerIcon(formation: formation, dancer: dancer, posx: dancer.position[0], posy: dancer.position[1])
+                }
+            }
+        }
+        .cornerRadius(20)
+        .scaledToFit()
+        .shadow(radius: 5, x: 0, y: 5)
     }
 }
 
 struct SingleFormationView_Previews: PreviewProvider {
     static var previews: some View {
-        SingleFormationView()
+        let dancer1 = DancerModel(position: [25.0, 25.0])
+        let dancer2 = DancerModel(position: [50.0, 50.0])
+        let formation = FormationModel(name: "Formation 1", dancers: [dancer1, dancer2])
+        SingleFormationView(formation: formation)
     }
 }
