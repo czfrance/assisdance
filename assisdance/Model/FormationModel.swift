fb88ee8b576c6f3207fe6bf8ddb9f2b9fbbc791b
Cynthia France <cf219@duke.edu>
2023-11-18 13:12:28 -0500

    update dancer functionality'

--- assisdance/Model/FormationModel.swift
+++ assisdance/Model/FormationModel.swift
@@ -7,12 +7,39 @@
 
 import Foundation
 
-struct FormationModel: Identifiable {
+struct FormationModel: Identifiable, Codable {
     let id: UUID
     let name: String
+//    var dancers: Dictionary<UUID, DancerModel>
+    var dancers: [DancerModel]
+//    var tag: Int
     
-    init(name: String) {
+    init(name: String, dancers: [DancerModel]) {
         self.id = UUID()
         self.name = name
+        self.dancers = dancers
+//        self.tag = tag
+    }
+    
+//    mutating func updateDancer(dId: UUID, x: Double, y: Double) {
+//        if dancers.keys.contains(dId) {
+//            let dancer = dancers[dId]
+//            let newDancer = DancerModel(id: dancer!.id, position: (x, y), path: dancer!.path)
+//            dancers[dId] = newDancer
+//        }
+//        else {
+//            print("dancer doesnt exist")
+//            return
+//        }
+//    }
+    
+    mutating func updateDancer(dId: UUID, x: Double, y: Double) {
+        for d in dancers {
+            if d.id == dId {
+                d.updatePosition(x: x, y: y)
+//                let newDancer = DancerModel(id: d.id, position: [x, y], path: d.path)
+//                dancers[i] = newDancer
+            }
+        }
     }
 }
