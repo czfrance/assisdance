60f4dfb1046a47ffce43d35873b06e4affb6121f
Cynthia France <cf219@duke.edu>
2023-11-21 01:21:44 -0500

    add dancer number identification

--- assisdance/Model/DancerModel.swift
+++ assisdance/Model/DancerModel.swift
@@ -9,12 +9,14 @@ import Foundation
 
 class DancerModel: Identifiable, Codable {
     let id: UUID
+    var number: Int
     var name: String = ""
     var position: [Double]
     var path: [Double]
     
-    init(id: UUID = UUID(), position: [Double], path: [Double] = []) {
+    init(id: UUID = UUID(), number: Int, position: [Double], path: [Double] = []) {
         self.id = id
+        self.number = number
         self.position = position
         self.path = path
     }
